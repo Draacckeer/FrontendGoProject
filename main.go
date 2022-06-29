0679f7af020e880a75327ff4154e8a2f95d39696
Draacckeer <u201922975@upc.edu.pe>
2022-06-29 02:36:36 -0500

    Add

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"text/template"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*"))
+var id = 0
 
 func main() {
 	/*response, err := http.Get(`https://gettingbetterapi.azurewebsites.net/api/v1/coaches`)
@@ -23,6 +28,9 @@ func main() {
 
 	http.HandleFunc("/", Start)
 	http.HandleFunc("/create", Create)
+	http.HandleFunc("/confirm-credentials", ConfirmCredentials)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/register", Register)
 	fmt.Println("Servidor corriendo...")
 
 	port := os.Getenv("PORT")
@@ -34,10 +42,67 @@ func main() {
 }
 
 func Start(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hola Dracker")
 	templates.ExecuteTemplate(w, "start", nil)
 }
 
+func Login(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login", nil)
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register", nil)
+}
+
 func Create(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "create", nil)
+	values := map[string]string{
+		"firstName":    "golang1",
+		"lastName":     "go2testx",
+		"selectedGame": "go3",
+		"nickName":     "go4",
+		"email":        "go5",
+		"password":     "go6",
+		"userImage":    "gox",
+		"bibliography": "goy"}
+	json_data, err := json.Marshal(values)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	//endpoint := "https://gettingbetterapi.azurewebsites.net/api/v1/coaches"
+	endpoint := "Dont Click"
+	resp, err := http.Post(endpoint, "application/json",
+		bytes.NewBuffer(json_data))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var res map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&res)
+
+	fmt.Println(res["json"])
+
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
+
+}
+
+func ConfirmCredentials(w http.ResponseWriter, r *http.Request) {
+	endpoint := "https://ksero.herokuapp.com/api/v1/users/auth/get-all"
+	resp, err := http.Get(endpoint)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
